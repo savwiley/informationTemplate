8af1a3b80c508957287441fa96a877dfa54a8890
Savannaha <savwiley@yahoo.com>
2022-04-06 00:07:18 -0500

    redirect to custom 404 page

--- index.js
+++ index.js
@@ -5,15 +5,33 @@ const fs = require("fs");
 http.createServer(function(req, res) {
   const site = url.parse(req.url, true);
   const file = "." + site.pathname;
+  if (site.pathname == "/") {
+    fs.readFile("index.html", function(err, data) {
+      if (err) {
+        console.warn(err);
+      } else {
+        console.log("Running...");
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+      }
+    })
+  };
   fs.readFile(file, function(err, data) {
     if (err) {
-      console.log(site);
-      res.writeHead(404, {"Content-Type": "text/html"});
-      return res.end("404 Not Found");
+      fs.readFile("404.html", (err, content) => {
+        if (err) {
+          console.warn(err);
+        }
+        console.log("404...");
+        res.writeHead(404, {"Content-Type": "text/html"});
+        return res.end(content);
+      })
+    } else {
+      console.log("Running...");
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(data);
+      return res.end();
     }
-    console.log(site);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    return res.end();
   });
 }).listen(8080);
