dd30bb9af9d4fca68db850dbcca8d32db69957c3
Savannaha <savwiley@yahoo.com>
2022-04-05 22:37:12 -0500

    fix 404 error

--- index.js
+++ index.js
@@ -2,28 +2,18 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+http.createServer(function(req, res) {
   const site = url.parse(req.url, true);
   const file = "." + site.pathname;
-  fs.readFile(file, (err, data) => {
+  fs.readFile(file, function(err, data) {
     if (err) {
+      console.log(site);
       res.writeHead(404, {"Content-Type": "text/html"});
-      res.write(data);
-      return res.end();
+      return res.end("404 Not Found");
     }
-    else {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write(data);
-      return res.end();
-    }
-  })
-}).listen("8080");
-
-
-/**
- * ERROR
- * node: _http_outgoing:722
- * https://stackoverflow.com/questions/59148769/post-using-request-promise-module-in-node-js-and-getting-http-outgoing-js618-th
- * 
- * TypeError [ERR_INVALID_ARG_TYPE]: The "chunk" argument must be of type string or an instance of Buffer or Uint8Array. Received undefined
- */
+    console.log(site);
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
