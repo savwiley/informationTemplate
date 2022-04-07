e5ace364df84ccb7192c4978bac8d375c4d79be6
Savannaha <savwiley@yahoo.com>
2022-04-06 19:01:34 -0500

    fix else/if to read end after write

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ http.createServer(function(req, res) {
         return res.end(data);
       }
     });
-  };
+  }
   //run index
-  if (site.pathname == "/") {
+  else if (site.pathname == "/") {
     fs.readFile("index.html", function(err, data) {
       if (err) {
         console.warn(err);
@@ -29,24 +29,26 @@ http.createServer(function(req, res) {
         return res.end();
       }
     })
-  };
+  }
   //run everything else
-  fs.readFile(file, function(err, data) {
-    if (err) {
-      //run custom 404
-      fs.readFile("404.html", (err, content) => {
-        if (err) {
-          console.warn(err);
-        }
-        console.log("404...");
-        res.writeHead(404, {"Content-Type": "text/html"});
-        return res.end(content);
-      })
-    } else {
-      console.log("Running...");
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write(data);
-      return res.end();
-    }
-  });
+  else {
+    fs.readFile(file, function(err, data) {
+      if (err) {
+        //run custom 404
+        fs.readFile("404.html", (err, content) => {
+          if (err) {
+            console.warn(err);
+          }
+          console.log("404...");
+          res.writeHead(404, {"Content-Type": "text/html"});
+          return res.end(content);
+        })
+      } else {
+        console.log("Running...");
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+      }
+    });
+  }
 }).listen(8080);
